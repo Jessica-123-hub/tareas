f4d1d32ba30ac5bb1fd512706a8f2a5d6ca82873
Jessica-123-hub <jessimar03@hotmail.com>
2021-01-06 06:30:55 +0000

    tareas w2

---
+++ operators.py
@@ -0,0 +1,17 @@
+age1 = 12
+age2 = 18
+print "age1 = ", age1
+print "age2 = ", age2
+print " + ", age1 + age2
+print " - ", age1 - age2
+print " * ", age1 * age2
+print " / ", age1 / age2
+print " % ", age2 % age1
+
+message = "Today was a beatiful day"
+firstname = "jessicaceballos"
+lastname = "jess"
+print message
+print "firstname = ", firstname
+print "lasttname = ", lastname
+print " + ", firstname + lastname
---
+++ variables.py
@@ -0,0 +1,21 @@
+# variables
+# Author : jessi
+# Oct 2020
+
+year = 2020
+sentence = "My name is jessi"
+sarah, bob, mike = 16, 20, 25
+name, age = "jessi", 29
+mary = fer = 24
+PI = 3.1416
+e=2.71
+
+# display values
+print "year = ", year
+print sentence
+print "sarah = ", sarah, "bob =", bob, "mike =", mike
+print "name =", name
+print "age =", age
+print mary, fer
+print PI
+print e
